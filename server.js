704a41697ae5db5e978425fa63ff1a6b1ce829d3
jgunn <j.s.gunn22@gmail.com>
2023-08-23 11:07:12 -0700

    sets up post

--- server.js
+++ server.js
@@ -1,18 +1,43 @@
 const express = require("express");
 const path = require("path");
+const db = require("./db/db.json");
 
 const PORT = 3001;
 
 const app = express();
 
+app.use(express.json());
 app.use(express.static("public"));
-
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/index.html"));
-});
 
 app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+
+app.post("/api/notes", (req, res) => {
+  console.info(`${req.method} was received`);
+
+  const { title, text } = req.body;
+
+  const newNote = {
+    title,
+    text,
+  };
+
+  const response = {
+    status: "success",
+    body: newNote,
+  };
+
+  console.log(response);
+  res.status(201).json(response);
+});
+
+app.get("/api/notes", (req, res) => {
+  res.send(db);
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
 app.listen(PORT, () => {
